97559e398c16a79da2b5d0eb1a5516643368aab7
Moritz Hölting <87192362+moritz-hoelting@users.noreply.github.com>
2023-02-28 19:42:19 +0100

    Geschichten hinzugefügt (Teil 1)

---
+++ components/geschichten/texte/glueck-auf.tsx
@@ -0,0 +1,157 @@
+import Geschichte from '../geschichte';
+
+export default function GeschichteGlueckAuf() {
+    return (
+        <Geschichte
+            id="geschichten/glueck_auf"
+            imgs={[
+                '0.jpg',
+                '1.jpg',
+                '2.jpg',
+                '3.jpg',
+                '4.jpg',
+                '5.jpg',
+                '6.jpg',
+                '7.jpg',
+                '8.jpg',
+                '9.jpg',
+                '10.jpg',
+                '11.jpg',
+                '12.jpg',
+                '13.jpg',
+                '14.jpg',
+                '15.jpg',
+                '16.jpg',
+                '17.jpg',
+                '18.jpg',
+                '19.jpg',
+                '20.jpg',
+                '21.jpg',
+                '22.jpg',
+                '23.jpg',
+            ]}
+        >
+            Löffelweise <br />
+            <br />
+            <h3>Glück auf!</h3>
+            <br />
+            Westfalen - zwei Extreme, die jeweils ihren ganz eigenen CHARME
+            ausstrahlen! <br />
+            <br />
+            Wir leben in OSTWESTFALEN, der ländlichen Gegend. Die Menschen
+            bezeichnen sich als bodenständig, traditionsbewusst, heimatliebend.
+            Agrarwirtschaftliche Flächen und eine dünne Besiedelung
+            charakterisieren die Landschaft. <br />
+            <br />
+            Im RUHRGEBIET dagegen hat sich mit seiner Arbeiterkultur,
+            Industriegeschichte und dem Strukturwandel eine hochmoderne
+            Gesellschaft entwickeln. Die Menschen leben am Puls der Zeit und
+            sind einem ständigen Wandel ausgesetzt. <br />
+            <br />
+            Grau- stickig - laut... <br />
+            Von wegen: Wir lieben Ausflüge in den Ruhrpott! <br />
+            <br />
+            Die Natur hat in vielen Bereichen die alten Industrieanlagen
+            zurückerobert. Allen voran der{' '}
+            <a href="https://landschaftspark.de/" target="_blank">
+                Landschaftspark Duisburg-Nord
+            </a>
+            . ROSTIG SCHIMMERNDE STAHLFÄCHEN und irre Rohrkonstellationen
+            inmitten großer Bäume und saftiger Grünflächen liefern eine
+            unbeschreibliche Kulisse für Hobbyfotografen. Noch spektakulärer
+            wird sie, wenn im Sommer der Park bunt illuminiert ist. Unbedingt
+            machen: Auf einen der HOCHÖFEN krachseln und die traumhafte Aussicht
+            genießen! <br />
+            Jette liebte bei unserem Besuch das Bouldern, Nicole die
+            künstlerischen Installationen... Und abends freuten wir uns aufs
+            MUSICAL - "
+            <a href="https://www.starlight-express.de/" target="_blank">
+                Starlight Express
+            </a>
+            "! Nicole hat es erstmalig 1989 mit der Schule besucht. Auch heute
+            noch beeindruckt die Geschwindigkeit und Perfektion, mit denen die
+            Schauspieler auf ihren Roller-Blades unterwegs sind. Und: Echt
+            witzig, dass die englische Lokomotive nun "Brexit" heißt. <br />
+            <br />
+            Als Moritz und Jette klein waren, wollten wir ihnen gern die
+            Geschichte ihrer Heimat näher bringen: Im ANSCHAUUNGSBERGWERK des{' '}
+            <a href="https://www.bergbaumuseum.de" target="_blank">
+                Deutschen Bergbaumuseums
+            </a>{' '}
+            in Bochum sahen wir imposante Maschinen und erlebten wir den
+            mühevollen Arbeitsalltag der Bergleute. In 20 Metern Tiefe baute man
+            dazu ein Streckennetz von etwa 2,5 km Länge, das einem realen
+            Bergwerk nachempfunden ist. Bei Mitmachangeboten & Workshops holten
+            wir uns nicht nur schwarze Nasen, sondern fühlten wir uns wie die
+            Grubis im Steinkohlebergwerk unter Tage. Doch wehe, wenn es hieß
+            "Hängen im Schacht!" <br />
+            <br />
+            Im Bauhaus-Jahr 2019 besuchten wir die{' '}
+            <a href="https://www.zollverein.de" target="_blank">
+                Zeche & Kokerei ZOLLVEREIN
+            </a>
+            . Neben der Geschichte des Steinkohlebergwerks und der
+            traditionellen Schwerindustrie richtete das UNESCO-Welterbe in
+            diesem Jahr den Fokus auf die Architekur der Neuen Sachlichkeit von
+            Schacht XII. Gestaltungskonzepte der BAUHAUS-ARCHITEKTUR in einem
+            industriellen Gesamtzusammenhang zu sehen, beeindruckte. Doch das
+            gewaltige Areal von rund einem Quadratkilometer war schon immer ein
+            Ort der Superlative. Vom Dach der KOHLENWÄSCHE erschließt sich ein
+            einzigartiges Panorama! In der Führung eines ehemaligen Kumpels
+            hörten wir lebendige SCHACHT-GESCHICHTEN. Wir erlebten, welchem Lärm
+            die Bergleute bei ihrer Arbeit ausgesetzt waren, erhielten eine vage
+            Vorstelltung von der Schwere der körperlichen Arbeit unter Tage für
+            das schwarze Gold. Das inmitten der Gebäude angesiedelte
+            "Red-Dot-Musem" führt passend dazu ein in das Design von
+            Alltags-Gegenständen, zeigt Preisgekröntes. <br />
+            <br />
+            Besondere Highlights für uns alle waren stets die Besuche des{' '}
+            <a href="https://www.gasometer.de/" target="_blank">
+                Gasometers Oberhausen
+            </a>
+            . <br />
+            Im Oktober 2013 bestaunten wir BIG AIR PACKAGE. 90 Meter hoch, ein
+            Durchmesser von 50 Metern und ein Volumen von 177.000 Kubikmetern:
+            Christos Kunstwerk im Innern des Gasometers ist die bis dato größte,
+            selbsttragende INNENRAUMSKULPTUR der Welt. 20.000 Quadratmeter
+            lichtdurchlässiges Gewebe werden von 4500 Metern Seil in Form
+            gehalten. Zwei Gebläse sorgen für einen konstanten Luftdruck im
+            Innern und halten das Kunstwerk aufrecht. Scheinwerfer, von oben
+            abgehängt, unterstützen die Leuchtkraft des weißen Stoffs. Das
+            Eintreten in den Ballon - ein Erlebnis, unbeschreiblich! <br />
+            Ähnlich beeindruckend: Die Ausstellung "Wunder der Natur" feierte im
+            Jahr 2017 BILDGEWALTIG das Leben von Tieren & Pflanzen. Wir
+            entdeckten die Genialität der biologischen Systeme - in der Realität
+            miskoskopisch winzig, auf Fotos und in Filmen majestätisch riesig
+            präsentiert. Besonderer Höhepunkt war die im gigantischen Innenraum
+            des Gasometers schwebende Installation einer monumentalen, 20 Meter
+            großen Skulptur der ERDKUGEL. Detailgenau und hochaufgelöst zeigten
+            Satellitenbilder die Schönheit unseres leuchtend blauen Planeten.{' '}
+            <br />
+            <br />
+            Eine völlig andere "Tour de Ruhr" erlebte Nicole 2015. Mit ihrem
+            Rennrad startete sie gemeinsam mit Ehemann Thorsten in Essen-Kettwig
+            rund um den POTT, 128km. Vorbei an schon bekannten
+            Sehenswürdigkeiten, entdeckte sie darüberhinaus jedoch noch viel,
+            viel mehr: die Schönheit des Baldeneysees, den Prunk der VILLA HÜGEL
+            & was die Familie Krupp für die Menschen der Region getan hat,
+            bestieg mit Klick-Schuhen die Rostentritte zum TETRAEDER, zählte die
+            Schlösser an allen Brücken der RUHR... Zu schön war die Wand des
+            Hotels über dem Frühstücks-Buffet: All die "komischen" (eben
+            westfälischen) Begriffe von Pinneken & Piselotten kannte Nicole seit
+            Kindertagen durch das Plattdeutsch ihrer Großeltern und konnte sie
+            direkt ins Hochdeutsche übersetzen. Sie verströmten ein vertrautes
+            Gefühl von HEIMAT! <br />
+            <br />
+            Trotz aller westfälischer Gegensätze - die Küche eint! Der typische
+            Westfale liebt rustikales Essen. Kohl & Pinkel, Reibekuchen mit
+            Schinken, Pumpernickel im Dessert - <br />
+            Löffelweise GLÜCK AUF!
+            <br />
+            <br />
+            <a href="/rezepte/?search=%2523westfälisch" target="_blank">
+                Zu den Rezepten
+            </a>
+        </Geschichte>
+    );
+}
--- pages/geschichten/index.tsx
+++ pages/geschichten/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head';
 import Footer from '../../components/footer';
-import Geschichte from '../../components/geschichten/geschichte';
 import Header from '../../components/geschichten/header';
+import GeschichteGlueckAuf from '../../components/geschichten/texte/glueck-auf';
+import GeschichteHobelbank from '../../components/geschichten/texte/hobelbank';
+import GeschichteMichel from '../../components/geschichten/texte/michel';
+import GeschichteRhabarbara from '../../components/geschichten/texte/rhabarbara';
+import GeschichteWeihnachten from '../../components/geschichten/texte/weihnachten';
 import NavigationBar from '../../components/navigation-bar';
 
 export default function GeschichtenPage() {
@@ -21,37 +25,11 @@ export default function GeschichtenPage() {
             <Header />
 
             <div id="geschichtenContainer">
-                <Geschichte
-                    id="geschichten/weihnachten"
-                    imgs={['mini.jpg', '1.jpg', '2.jpg']}
-                >
-                    <h3>Geschichte</h3>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Saepe deserunt fugit, suscipit beatae, fugiat repellendus
-                    recusandae tempora maxime dolore explicabo voluptatibus
-                    itaque autem et? Et atque quisquam reiciendis laboriosam
-                    doloremque? Lorem ipsum, dolor sit amet consectetur
-                    adipisicing elit. Sit enim eveniet quam corrupti iste atque
-                    facere possimus, quaerat, cumque id nesciunt est, temporibus
-                    suscipit? Illum iste reiciendis veniam aspernatur, vel
-                    exercitationem quisquam sapiente magnam quasi possimus
-                    officiis ad dolore, ducimus beatae similique tenetur
-                    corrupti ipsum nulla quibusdam laborum ullam velit culpa
-                    necessitatibus mollitia. Vel assumenda similique culpa,
-                    consequatur optio sapiente dolore autem. Provident deleniti,
-                    numquam excepturi, ipsa facilis, quod cum iste magnam vitae
-                    maiores totam! Adipisci, reiciendis debitis earum eaque
-                    consequuntur repellat, ratione ut, voluptates sunt dolorem
-                    ipsum similique culpa. Odit consectetur cum aliquam veniam
-                    facilis deserunt vel exercitationem porro! Lorem, ipsum
-                    dolor sit amet consectetur adipisicing elit. Error, ipsa
-                    dolor. Voluptas pariatur est quis architecto facilis harum.
-                    Obcaecati, quibusdam illo consequatur magnam at blanditiis
-                    nam eos delectus ab magni velit voluptas nobis quod
-                    asperiores debitis error ut sit nemo assumenda doloremque.
-                    Possimus, voluptas temporibus delectus quis magnam deleniti
-                    eveniet!
-                </Geschichte>
+                <GeschichteWeihnachten />
+                <GeschichteHobelbank />
+                <GeschichteMichel />
+                <GeschichteRhabarbara />
+                <GeschichteGlueckAuf />
             </div>
 
             <Footer />
