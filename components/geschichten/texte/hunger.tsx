40560dba98421e6733570a64a2f9d1c73beb89f3
Moritz Hölting <87192362+moritz-hoelting@users.noreply.github.com>
2023-03-05 14:46:16 +0100

    Geschichte Hunger Schriftart korrigiert

--- components/geschichten/texte/hunger.tsx
+++ components/geschichten/texte/hunger.tsx
@@ -3,7 +3,7 @@ import Geschichte from '../geschichte';
 export default function GeschichteHunger() {
     return (
         <Geschichte id="geschichten" imgs={['hunger.jpg']}>
-            <h2>HUNGER ?!</h2>
+            <h3>HUNGER ?!</h3>
             <a href="/rezepte">Hier geht's zu den Rezepten...</a>
         </Geschichte>
     );
