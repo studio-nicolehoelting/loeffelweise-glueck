150f5abc3f1a01090ef0e27b1aa04ae90cad697f
Moritz Hölting <87192362+moritz-hoelting@users.noreply.github.com>
2023-10-08 20:09:51 +0200

    Reverted images to webp for edge browser support

---
+++ src/pages/rezepte.json.ts
@@ -0,0 +1,23 @@
+import { getCollection } from "astro:content";
+import type { APIRoute } from "astro";
+import { getImage } from "astro:assets";
+
+export const GET: APIRoute = async () => {
+    const data = await getCollection("rezepte").then(
+        async (collection) =>
+            await Promise.all(
+                collection.map(async (item) => ({
+                    name: item.data.name,
+                    slug: item.slug,
+                    img: await getImage({
+                        src: item.data.bild,
+                        quality: "mid",
+                    }).then((img) => img.src),
+                    datum: item.data.datum,
+                    praesentiert: item.data.praesentiert,
+                    tags: item.data.tags,
+                }))
+            )
+    );
+    return new Response(JSON.stringify(data));
+};
---
+++ src/pages/rezepte/[rezept].json.ts
@@ -0,0 +1,44 @@
+import type { APIRoute, GetStaticPaths } from "astro";
+import { getImage } from "astro:assets";
+import { getCollection, getEntryBySlug } from "astro:content";
+
+export const getStaticPaths = (async () => {
+    const rezepte = await getCollection("rezepte");
+    return rezepte.map((rezept) => ({
+        params: {
+            rezept: rezept.slug,
+        },
+        props: {
+            slug: rezept.slug,
+        },
+    }));
+}) satisfies GetStaticPaths;
+
+export const GET: APIRoute = async ({ params }) => {
+    const { rezept: slug } = params;
+    if (!slug) throw new Error("No slug provided");
+
+    const rezept = await getEntryBySlug("rezepte", slug);
+    if (!rezept) {
+        throw new Error(`Rezept ${slug} not found`);
+    }
+
+    const img = await getImage({
+        src: rezept.data.bild,
+        quality: "mid",
+    }).then((img) => img.src);
+
+    return new Response(
+        JSON.stringify({
+            name: rezept.data.name,
+            slug: rezept.slug,
+            img,
+            datum: rezept.data.datum,
+            tags: rezept.data.tags,
+            zutaten: rezept.data.zutaten,
+            zubereitung: rezept.body,
+            kommentar: rezept.data.kommentar,
+            praesentiert: rezept.data.praesentiert,
+        })
+    );
+};
